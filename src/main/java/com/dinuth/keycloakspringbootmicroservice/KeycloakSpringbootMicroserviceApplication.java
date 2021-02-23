c14b6de53217f9ea65cf4aebce0b03f2b7dfd552
Renato Alves Felix <renatofelix.redhat@gmail.com>
2021-02-22 21:42:41 -0300

    Initial

--- src/main/java/com/dinuth/keycloakspringbootmicroservice/KeycloakSpringbootMicroserviceApplication.java
+++ src/main/java/com/dinuth/keycloakspringbootmicroservice/KeycloakSpringbootMicroserviceApplication.java
@@ -2,12 +2,14 @@ package com.dinuth.keycloakspringbootmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// https://stackoverflow.com/questions/30761253/remove-using-default-security-password-on-spring-boot
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class KeycloakSpringbootMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KeycloakSpringbootMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakSpringbootMicroserviceApplication.class, args);
+    }
 
 }
--- src/main/java/com/dinuth/keycloakspringbootmicroservice/controller/TestController.java
+++ src/main/java/com/dinuth/keycloakspringbootmicroservice/controller/TestController.java
@@ -5,32 +5,32 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
     @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
     public ResponseEntity<String> getAnonymous() {
         return ResponseEntity.ok("Hello Anonymous");
     }
 
-    @RolesAllowed("user")
+//    @RolesAllowed("user")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public ResponseEntity<String> getUser(@RequestHeader String Authorization) {
+    public ResponseEntity<String> getUser(@RequestHeader String jwtToken) {
         return ResponseEntity.ok("Hello User");
     }
 
-    @RolesAllowed("admin")
+//    @RolesAllowed("admin")
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public ResponseEntity<String> getAdmin(@RequestHeader String Authorization) {
+    public ResponseEntity<String> getAdmin(@RequestHeader String jwtToken) {
         return ResponseEntity.ok("Hello Admin");
     }
 
-    @RolesAllowed({ "admin", "user" })
+//    @RolesAllowed({ "admin", "user" })
     @RequestMapping(value = "/all-user", method = RequestMethod.GET)
-    public ResponseEntity<String> getAllUser(@RequestHeader String Authorization) {
+    public ResponseEntity<String> getAllUser(@RequestHeader String jwtToken) {
         return ResponseEntity.ok("Hello All User");
     }
 
